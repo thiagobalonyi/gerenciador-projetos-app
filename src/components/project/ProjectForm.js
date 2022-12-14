3de1dd5e47f0d41f39be7cb7157dc7c909aa8653
Thiago Balonyi <thiagocandal@gmail.com>
2022-12-14 16:13:47 -0300

    add: API/Banco de dados para options select do form

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
@@ -5,12 +7,40 @@ import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
 
+    const [categories, setCategories] = useState([])
+
+//Fetch API e Promises
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+//Fetch API e Promises
     return (
         <form className={styles.form}>
-            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
-            <Input type="number" text="orçamento do projeto" name="budget" placeholder="Insira o orçamento total" />
+            <Input 
+            type="text" 
+            text="Nome do projeto" 
+            name="name" 
+            placeholder="Insira o nome do projeto" />
 
-            <Select name="category_id" text="Selecione a categoria" />
+            <Input 
+            type="number" 
+            text="orçamento do projeto" 
+            name="budget" 
+            placeholder="Insira o orçamento total" />
+
+            <Select 
+            name="category_id" 
+            text="Selecione a categoria" options={categories} />
 
             <SubmitButton text={btnText} />
         </form>
