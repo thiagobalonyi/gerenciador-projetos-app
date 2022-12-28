cbc9fbb46dc8ea52ea49dc3cbf7521a4ce559a11
Thiago Balonyi <thiagocandal@gmail.com>
2022-12-28 12:03:49 -0300

    add: Adicionando serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from 'uuid'
+
 import styles from "./Project.module.css"
 
 import { useParams } from "react-router-dom"
@@ -6,14 +8,15 @@ import {useState, useEffect} from 'react'
 import Loading from '../layouts/Loading'
 import Message from '../layouts/Message'
 import Container from '../layouts/Container'
-
 import ProjectForm from '../project/ProjectForm'
+import ServiceForm from '../service/ServiceForm'
 
 function Project() {
 
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -34,6 +37,8 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+
         // budget validation
         if (project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
@@ -59,8 +64,51 @@ function Project() {
         .catch(err => console.log(err))
     }
 
+    function createService(project) {
+        setMessage('')
+
+        // last service
+        const lastService = project.services[project.services.length - 1]
+    
+        lastService.id = uuidv4()
+    
+        const lastServiceCost = lastService.cost
+    
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+    
+        // maximum value validation
+        if (newCost > parseFloat(project.budget)) {
+            setMessage('Orçamento ultrapassado, verifique o valor do serviço!')
+            setType('error')
+            project.services.pop()
+            return false
+        }
+
+        // add service cost to project total cost
+        project.cost = newCost
+
+        // Update project
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            // exibir serviços
+            console.log(data)
+        })
+        .catch((err) => console.log(err))
+    }
+
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return <>{project.name ? 
@@ -70,8 +118,8 @@ function Project() {
                 <div className={styles.details_container}>
                     <h1>Projeto: {project.name}</h1>
                     <button className={styles.btn} onClick={toggleProjectForm}>
-                        {!showProjectForm ? 'Editar projeto' : 'Fechar'}
-                        </button>
+                    {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+                    </button>
                         {!showProjectForm ? (
                             <div className={styles.project_info}>
                                 <p>
@@ -93,6 +141,23 @@ function Project() {
                             </div>
                         )}
                 </div>
+                <div className={styles.service_form_container}>
+                    <h2>Adicione um serviço</h2>
+                    <button className={styles.btn} onClick={toggleServiceForm}>
+                    {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                    </button>
+                    <div className={styles.project_info}>
+                            {showServiceForm && <ServiceForm 
+                            handleSubmit={createService}
+                            btnText="Adicionar Serviço"
+                            projectData={project}
+                            />}
+                    </div>
+                </div>
+                <h2>Serviços</h2>
+                <Container customClass="start">
+                    <p>Itens de serviços</p>
+                </Container>
             </Container>
         </div>
         : <Loading /> } </>
